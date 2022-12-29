78c8ad04e37da822d70c5ef8a5968d53a164dcc4
Hiromi ISHII <konn.jinro@gmail.com>
2022-12-29 20:46:48 +0900

    Image texture

--- src/RayTracing/Texture/Image.hs
+++ src/RayTracing/Texture/Image.hs
@@ -1,17 +1,41 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GHC2021 #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-module RayTracing.Texture.Image (ImageTexture, ImageTexture' (..)) where
+module RayTracing.Texture.Image (ImageTexture, ImageTexture' (..), loadImageTexture) where
 
+import Control.Lens ((%~))
+import Data.Coerce (coerce)
+import Data.Function ((&))
+import Data.Functor ((<&>))
 import Data.Image.Types (Pixel (..), RGB)
+import Data.Image.Types qualified as AAA
 import Data.Massiv.Array qualified as M
-import Data.Massiv.Array.IO (Image)
+import Data.Massiv.Array.IO (Image, liftPixel, readImageAuto, unColorRGB)
+import Data.Massiv.Array.IO qualified as C
+import Data.Ord (clamp)
 import GHC.Generics (Generic)
+import Linear
+import Linear.Affine (Point (..))
+import RIO (MonadIO)
 import RayTracing.Texture
 
-newtype ImageTexture' r = ImageTexture {runImageTexture' :: Image r RGB Double}
+newtype ImageTexture' r = ImageTexture {image :: Image r RGB Double}
   deriving (Generic)
 
 type ImageTexture = ImageTexture' M.S
 
-instance M.Source r (Pixel RGB Double) => Texture (ImageTexture' r) where
+loadImageTexture :: MonadIO m => FilePath -> m ImageTexture
+loadImageTexture =
+  fmap (ImageTexture . M.computeP . M.map (liftPixel unColorRGB))
+    . readImageAuto @M.S @(C.SRGB 'C.Linear)
+
+instance M.Manifest r (Pixel RGB Double) => Texture (ImageTexture' r) where
+  {-# INLINE colorAt #-}
+  colorAt ImageTexture {..} p =
+    let P (V2 u v) = p <&> clamp (0, 1) & _y %~ (1 -)
+        M.Sz2 h w = M.size image
+        i = floor (2 * u * fromIntegral (w - 1)) `quot` w
+        j = floor (2 * v * fromIntegral (h - 1)) `quot` h
+     in const $ coerce $ image M.! (i M.:. j)
