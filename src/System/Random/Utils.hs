97e147de1d1a9b921e77d53b0a857af6be370f86
Hiromi ISHII <konn.jinro@gmail.com>
2022-12-31 16:19:55 +0900

    Expelled

--- src/System/Random/Utils.hs
+++ src/System/Random/Utils.hs
@@ -18,7 +18,7 @@ we use the spherical coordinate instead; i.e.
 sampling θ ∈ [0, π], φ ∈ [-π, π].
 -}
 randomPointOnUnitSphere :: RandomGen g => g -> (Dir V3 Double, g)
-randomPointOnUnitSphere = flip runSTGen $ \mg -> do
+randomPointOnUnitSphere = flip runStateGen $ \mg -> do
   theta <- (pi *) <$> randomRM (0, 1.0) mg
   phi <- (pi *) <$> randomRM (-1.0, 1.0) mg
   pure $ unsafeDir $ V3 (sin theta * cos phi) (sin theta * sin phi) (cos theta)
@@ -29,15 +29,15 @@ we use the spherical coordinate instead; i.e.
 sampling θ ∈ [0, π/2], φ ∈ [-π, π].
 -}
 randomPointOnUnitHemisphere :: RandomGen g => Dir V3 Double -> g -> (Dir V3 Double, g)
-randomPointOnUnitHemisphere normal g = runSTGen g $ \mg -> do
-  pt <- applySTGen randomPointOnUnitSphere mg
+randomPointOnUnitHemisphere normal g = runStateGen g $ \mg -> do
+  pt <- applyRandomGenM randomPointOnUnitSphere mg
   pure $
     if unDir pt `dot` unDir normal > 0
       then pt
       else negate <$> pt
 
 randomPointInUnitDisk :: RandomGen g => g -> (Point V2 Double, g)
-randomPointInUnitDisk = flip runSTGen $ \g -> do
+randomPointInUnitDisk = flip runStateGen $ \g -> do
   r <- randomRM (0, 1) g
   theta <- (2 * pi *) <$> randomRM (-1, 1) g
   pure $ P $ V2 (r * cos theta) (r * sin theta)
