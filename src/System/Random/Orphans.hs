9146498cfcdb5f8f502ff26f0ff7ec1e9bbdedbc
Hiromi ISHII <konn.jinro@gmail.com>
2023-01-04 23:41:00 +0900

    Attempt for unboxed StdGen Mutable refs (WIP)

--- src/System/Random/Orphans.hs
+++ src/System/Random/Orphans.hs
@@ -1,13 +1,24 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE GHC2021 #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module System.Random.Orphans () where
 
+import Control.Monad ((<$!>))
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Maybe
 import Control.Monad.Trans.Writer.CPS (WriterT)
+import Data.Vector.Generic qualified as G
+import Data.Vector.Generic.Mutable qualified as MG
+import Data.Vector.Unboxed qualified as U
+import Data.Vector.Unboxed.Deriving (derivingUnbox)
+import Data.Word (Word64)
+import System.Random.Internal
+import System.Random.SplitMix
 import System.Random.Stateful
 
 instance {-# INCOHERENT #-} StatefulGen g m => StatefulGen g (MaybeT m) where
@@ -49,3 +60,55 @@ instance {-# INCOHERENT #-} (StatefulGen g m) => StatefulGen g (WriterT w m) whe
 instance {-# OVERLAPPING #-} (RandomGenM g r m) => RandomGenM g r (WriterT w m) where
   applyRandomGenM = fmap lift . applyRandomGenM
   {-# INLINE applyRandomGenM #-}
+
+data instance U.Vector SMGen = V_SMGen !Int (U.Vector Word64)
+
+data instance U.MVector s SMGen = MV_SMGen !Int (U.MVector s Word64)
+
+instance G.Vector U.Vector SMGen where
+  basicUnsafeFreeze = \case
+    MV_SMGen len mu -> V_SMGen len <$!> G.basicUnsafeFreeze mu
+  {-# INLINE basicUnsafeFreeze #-}
+  basicUnsafeThaw = \case
+    V_SMGen len u -> MV_SMGen len <$!> G.basicUnsafeThaw u
+  {-# INLINE basicUnsafeThaw #-}
+  basicLength = \case
+    V_SMGen len _ -> len
+  {-# INLINE basicLength #-}
+  basicUnsafeSlice off count (V_SMGen _ u) =
+    V_SMGen count $ G.basicUnsafeSlice (2 * off) (2 * count) u
+  {-# INLINE basicUnsafeSlice #-}
+  basicUnsafeIndexM (V_SMGen _ u) i =
+    let !n = 2 * i
+     in seedSMGen <$!> G.basicUnsafeIndexM u n <*> G.basicUnsafeIndexM u n
+  {-# INLINE basicUnsafeIndexM #-}
+
+instance MG.MVector U.MVector SMGen where
+  basicLength = \case
+    MV_SMGen len _ -> len
+  {-# INLINE basicLength #-}
+  basicUnsafeSlice off count (MV_SMGen _ u) =
+    MV_SMGen count $ MG.basicUnsafeSlice (2 * off) (2 * count) u
+  {-# INLINE basicUnsafeSlice #-}
+  basicOverlaps (MV_SMGen _ u) (MV_SMGen _ v) = MG.basicOverlaps u v
+  {-# INLINE basicOverlaps #-}
+  basicUnsafeNew n = MV_SMGen n <$> MG.basicUnsafeNew (2 * n)
+  {-# INLINE basicUnsafeNew #-}
+  basicInitialize (MV_SMGen _ u) = MG.basicInitialize u
+  {-# INLINE basicInitialize #-}
+  basicUnsafeRead (MV_SMGen _ mu) i = do
+    let !off = 2 * i
+    seedSMGen <$!> MG.basicUnsafeRead mu off <*> MG.basicUnsafeRead mu (off + 1)
+  {-# INLINE basicUnsafeRead #-}
+  basicUnsafeWrite (MV_SMGen _ mu) i gen = do
+    let !off = 2 * i
+        (!l, !r) = unseedSMGen gen
+    MG.basicUnsafeWrite mu off l
+    MG.basicUnsafeWrite mu (off + 1) r
+  {-# INLINE basicUnsafeWrite #-}
+
+derivingUnbox
+  "StdGen"
+  [t|StdGen -> SMGen|]
+  [|\(StdGen sg) -> sg|]
+  [|StdGen|]
---
+++ src/System/Random/Stateful/STUnbox.hs
@@ -0,0 +1,93 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GHC2021 #-}
+{-# LANGUAGE ImpredicativeTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module System.Random.Stateful.STUnbox (
+  STUGen (..),
+  STUGenM (..),
+  applySTUGen,
+  runSTUGen,
+  runSTUGen_,
+) where
+
+import Control.Monad.ST.Strict (RealWorld, ST, runST)
+import Control.Monad.Trans.Maybe (MaybeT)
+import Data.Vector.Unboxed qualified as U
+import Data.Vector.Unboxed.Mutable qualified as MU
+import GHC.Generics
+import RIO (NFData, PrimMonad (PrimState), Storable)
+import System.Random.Orphans ()
+import System.Random.Stateful
+
+newtype STUGen g = STUGen {unSTUGen :: g}
+  deriving (Show, Eq, Ord, Generic, Generic1, Functor, Foldable, Traversable)
+  deriving newtype (RandomGen, NFData, Storable)
+
+instance (PrimMonad m, U.Unbox g, RandomGen g) => FrozenGen (STUGen g) m where
+  {-# SPECIALIZE instance FrozenGen (STUGen StdGen) IO #-}
+  {-# SPECIALIZE instance FrozenGen (STUGen StdGen) (ST s) #-}
+  {-# SPECIALIZE instance FrozenGen (STUGen StdGen) (MaybeT (ST s)) #-}
+  {-# SPECIALIZE instance FrozenGen (STUGen StdGen) (MaybeT IO) #-}
+  type MutableGen (STUGen g) m = STUGenM g (PrimState m)
+  freezeGen = fmap STUGen . flip MU.unsafeRead 0 . unSTUGenM
+  {-# INLINE freezeGen #-}
+  thawGen = fmap STUGenM . U.unsafeThaw . U.singleton . unSTUGen
+  {-# INLINE thawGen #-}
+
+applySTUGen :: (PrimMonad m, U.Unbox g) => (g -> (a, g)) -> STUGenM g (PrimState m) -> m a
+{-# INLINE applySTUGen #-}
+{-# SPECIALIZE INLINE applySTUGen :: (StdGen -> (a, StdGen)) -> STUGenM StdGen s -> ST s a #-}
+{-# SPECIALIZE INLINE applySTUGen :: (StdGen -> (a, StdGen)) -> STUGenM StdGen s -> MaybeT (ST s) a #-}
+{-# SPECIALIZE INLINE applySTUGen :: (StdGen -> (a, StdGen)) -> STUGenM StdGen RealWorld -> IO a #-}
+{-# SPECIALIZE INLINE applySTUGen :: (StdGen -> (a, StdGen)) -> STUGenM StdGen RealWorld -> MaybeT IO a #-}
+applySTUGen f (STUGenM ref) = do
+  g <- MU.unsafeRead ref 0
+  case f g of
+    (!a, !g') -> a <$ MU.unsafeWrite ref 0 g'
+
+instance
+  (PrimMonad m, RandomGen g, U.Unbox g, PrimState m ~ s) =>
+  StatefulGen (STUGenM g s) m
+  where
+  {-# SPECIALIZE instance StatefulGen (STUGenM StdGen s) (ST s) #-}
+  {-# SPECIALIZE instance StatefulGen (STUGenM StdGen s) (MaybeT (ST s)) #-}
+  {-# SPECIALIZE instance StatefulGen (STUGenM StdGen RealWorld) IO #-}
+  {-# SPECIALIZE instance StatefulGen (STUGenM StdGen RealWorld) (MaybeT IO) #-}
+  uniformWord32R r = applySTUGen (genWord32R r)
+  {-# INLINE uniformWord32R #-}
+  uniformWord64R r = applySTUGen (genWord64R r)
+  {-# INLINE uniformWord64R #-}
+  uniformWord8 = applySTUGen genWord8
+  {-# INLINE uniformWord8 #-}
+  uniformWord16 = applySTUGen genWord16
+  {-# INLINE uniformWord16 #-}
+  uniformWord32 = applySTUGen genWord32
+  {-# INLINE uniformWord32 #-}
+  uniformWord64 = applySTUGen genWord64
+  {-# INLINE uniformWord64 #-}
+  uniformShortByteString n = applySTUGen (genShortByteString n)
+
+instance
+  (PrimState m ~ s, RandomGen g, U.Unbox g, PrimMonad m) =>
+  RandomGenM (STUGenM g s) g m
+  where
+  {-# SPECIALIZE instance RandomGenM (STUGenM StdGen s) StdGen (ST s) #-}
+  {-# SPECIALIZE instance RandomGenM (STUGenM StdGen s) StdGen (MaybeT (ST s)) #-}
+  {-# SPECIALIZE instance RandomGenM (STUGenM StdGen RealWorld) StdGen IO #-}
+  {-# SPECIALIZE instance RandomGenM (STUGenM StdGen RealWorld) StdGen (MaybeT IO) #-}
+  applyRandomGenM = applySTUGen
+  {-# INLINE applyRandomGenM #-}
+
+newtype STUGenM g s = STUGenM {unSTUGenM :: U.MVector s g}
+
+runSTUGen :: (RandomGen g, U.Unbox g) => g -> (forall s. STUGenM g s -> ST s a) -> (a, g)
+{-# INLINE runSTUGen #-}
+{-# SPECIALIZE INLINE runSTUGen :: StdGen -> (forall s. STUGenM StdGen s -> ST s a) -> (a, StdGen) #-}
+runSTUGen g action = unSTUGen <$> runST (withMutableGen (STUGen g) action)
+
+runSTUGen_ :: (RandomGen g, U.Unbox g) => g -> (forall s. STUGenM g s -> ST s a) -> a
+{-# SPECIALIZE INLINE runSTUGen_ :: StdGen -> (forall s. STUGenM StdGen s -> ST s a) -> a #-}
+{-# INLINE runSTUGen_ #-}
+runSTUGen_ = fmap fst . runSTUGen
